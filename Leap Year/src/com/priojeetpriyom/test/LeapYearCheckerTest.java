f0cb9460bbe60965099091d413a08324d1f30621
Priojeet Das Priyom <priojeetpriyom@gmail.com>
2020-05-24 13:59:02 +0600

    Refactor code

--- Leap Year/src/com/priojeetpriyom/test/LeapYearCheckerTest.java
+++ Leap Year/src/com/priojeetpriyom/test/LeapYearCheckerTest.java
@@ -1,34 +1,36 @@
 package com.priojeetpriyom.test;
 
 import com.priojeetpriyom.main.LeapYearChecker;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class LeapYearCheckerTest {
+    private static LeapYearChecker leapYearChecker;
 
+    @BeforeAll
+    public static void init() {
+        leapYearChecker = new LeapYearChecker();
+    }
     @Test
     void when4ThenLeapYear() {
-        LeapYearChecker leapYearChecker = new LeapYearChecker();
         assertTrue(leapYearChecker.isLeapYear(4));
     }
 
     @Test
     void when1ThenNotLeapYear() {
-        LeapYearChecker leapYearChecker = new LeapYearChecker();
         assertFalse(leapYearChecker.isLeapYear(1));
     }
 
     @Test
     void when5ThenNotLeapYear() {
-        LeapYearChecker leapYearChecker = new LeapYearChecker();
         assertFalse(leapYearChecker.isLeapYear(5));
     }
 
     @Test
     void whenDivisibleBy100ThenNotLeapYear() {
-        LeapYearChecker leapYearChecker = new LeapYearChecker();
         assertFalse(leapYearChecker.isLeapYear(100), "Failed for 100");
         assertFalse(leapYearChecker.isLeapYear(200), "Failed for 200");
         assertFalse(leapYearChecker.isLeapYear(1000), "Failed for 1000");
