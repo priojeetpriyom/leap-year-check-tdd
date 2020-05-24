620cc1341874f74ebab781ecda82b0aae089ce41
Priojeet Das Priyom <priojeetpriyom@gmail.com>
2020-05-24 13:06:57 +0600

    Add condition for year 1

--- Leap Year/src/com/priojeetpriyom/main/LeapYearChecker.java
+++ Leap Year/src/com/priojeetpriyom/main/LeapYearChecker.java
@@ -2,6 +2,9 @@ package com.priojeetpriyom.main;
 
 public class LeapYearChecker {
     public Boolean isLeapYear(int year) {
+        if(year == 1) {
+            return false;
+        }
         return true;
     }
 }
--- Leap Year/src/com/priojeetpriyom/test/LeapYearCheckerTest.java
+++ Leap Year/src/com/priojeetpriyom/test/LeapYearCheckerTest.java
@@ -3,6 +3,7 @@ package com.priojeetpriyom.test;
 import com.priojeetpriyom.main.LeapYearChecker;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class LeapYearCheckerTest {
@@ -11,6 +12,12 @@ public class LeapYearCheckerTest {
     void when4ThenLeapYear() {
         LeapYearChecker leapYearChecker = new LeapYearChecker();
         assertTrue(leapYearChecker.isLeapYear(4));
+    }
+
+    @Test
+    void when1ThenNotLeapYear() {
+        LeapYearChecker leapYearChecker = new LeapYearChecker();
+        assertFalse(leapYearChecker.isLeapYear(1));
     }
 
 }
