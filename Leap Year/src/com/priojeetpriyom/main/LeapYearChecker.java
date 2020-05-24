297c7e2498b5c68c29d485fc4bc08e5b6cee776e
Priojeet Das Priyom <priojeetpriyom@gmail.com>
2020-05-24 18:34:41 +0600

    Add support for arbitary long year

--- Leap Year/src/com/priojeetpriyom/main/LeapYearChecker.java
+++ Leap Year/src/com/priojeetpriyom/main/LeapYearChecker.java
@@ -1,19 +1,27 @@
 package com.priojeetpriyom.main;
 
+import java.math.BigInteger;
+
 public class LeapYearChecker {
-    public Boolean isLeapYear(int year) {
-        if(year % 400 == 0) {
+    public Boolean isLeapYear(String yearString) {
+        BigInteger year = new BigInteger(yearString);
+
+        if(isDivisible(year, new BigInteger("400") ) ) {
             return true;
         }
 
-        if(year % 100 == 0) {
+        if(isDivisible(year, new BigInteger("100"))) {
             return false;
         }
 
-        if(year % 4 == 0) {
+        if(isDivisible(year, new BigInteger("4"))) {
             return true;
         }
 
         return false;
+    }
+
+    private boolean isDivisible(BigInteger dividend, BigInteger divisor) {
+        return dividend.mod(divisor).equals(new BigInteger("0"));
     }
 }
--- Leap Year/src/com/priojeetpriyom/test/LeapYearCheckerTest.java
+++ Leap Year/src/com/priojeetpriyom/test/LeapYearCheckerTest.java
@@ -16,31 +16,41 @@ public class LeapYearCheckerTest {
     }
     @Test
     void when4ThenLeapYear() {
-        assertTrue(leapYearChecker.isLeapYear(4));
+        assertTrue(leapYearChecker.isLeapYear("4"));
     }
 
     @Test
     void when1ThenNotLeapYear() {
-        assertFalse(leapYearChecker.isLeapYear(1));
+        assertFalse(leapYearChecker.isLeapYear("1"));
     }
 
     @Test
     void when5ThenNotLeapYear() {
-        assertFalse(leapYearChecker.isLeapYear(5));
+        assertFalse(leapYearChecker.isLeapYear("5"));
     }
 
     @Test
     void whenDivisibleBy100ThenNotLeapYear() {
-        assertFalse(leapYearChecker.isLeapYear(100), "Failed for 100");
-        assertFalse(leapYearChecker.isLeapYear(200), "Failed for 200");
-        assertFalse(leapYearChecker.isLeapYear(1000), "Failed for 1000");
+        assertFalse(leapYearChecker.isLeapYear("100"), "Failed for 100");
+        assertFalse(leapYearChecker.isLeapYear("200"), "Failed for 200");
+        assertFalse(leapYearChecker.isLeapYear("1000"), "Failed for 1000");
     }
 
     @Test
     void whenDivisibleBy400ThenLeapYear() {
-        assertTrue(leapYearChecker.isLeapYear(400), "Failed for 400");
-        assertTrue(leapYearChecker.isLeapYear(800), "Failed for 800");
-        assertTrue(leapYearChecker.isLeapYear(4000), "Failed for 4000");
+        assertTrue(leapYearChecker.isLeapYear("400"), "Failed for 400");
+        assertTrue(leapYearChecker.isLeapYear("800"), "Failed for 800");
+        assertTrue(leapYearChecker.isLeapYear("4000"), "Failed for 4000");
+    }
+
+    @Test
+    void whenLeapYearGreaterThanIntRangeThenLeapYear() {
+        assertTrue(leapYearChecker.isLeapYear("4000000000000"), "Failed For 4000000000000");
+    }
+
+    @Test
+    void whenNotLeapYearGreaterThanIntRangeThenLeapYear() {
+        assertFalse(leapYearChecker.isLeapYear("4000000000000111"), "Failed For 4000000000000111");
     }
 
 }
